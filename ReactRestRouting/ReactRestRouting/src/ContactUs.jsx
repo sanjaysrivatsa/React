30b5736c1ab3bc297910d2c89cd6e1770ee1cf28
sanjaysrivatsa <sanjsriv@yahoo.com>
2025-03-09 20:52:59 +0530

    Delete ReactRestRouting/ReactRestRouting directory

--- ReactRestRouting/ReactRestRouting/src/ContactUs.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-var emailids=['kishorekumar@johndeere.com', 'sanjsriv@yahoo.com', 'maheshshitut@johndeere.com'];
-class ContactUs extends React.PureComponent {
-   render() {
-      return <h1> Contact us </h1>
-   }
-}
-export default ContactUs;
